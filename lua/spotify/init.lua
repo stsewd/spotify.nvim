ef2c90d6fe8ecc2debddb54e85e8275e3d71d6b4
Santos Gallegos <stsewd@proton.me>
2024-11-09 19:46:20 -0500

    Better implementation + Snacks.notifier support

--- lua/spotify/init.lua
+++ lua/spotify/init.lua
@@ -4,19 +4,18 @@ local config = {
   refresh_interval = 50,
   cycle_speed = 5,
   show_status_after_action = true,
-  -- wait_time?
-  timeout = nil,
+  timeout = 3000,
   render = {
     -- return_array=false
     -- template
     -- symbols
     -- progress_bar_width
     -- width
-  }
+  },
 }
 
-local function completion (arglead, cmdline, cursorpos)
-  local result = {"play", "pause", "play/pause"}
+local function completion(arglead, cmdline, cursorpos)
+  local result = { "play", "pause", "play/pause" }
   -- local i = 1
   -- for action in ipairs(get_actions())
   --   result[i] = action
@@ -25,7 +24,10 @@ local function completion (arglead, cmdline, cursorpos)
 end
 
 -- Local variable to keep track of the current open notification.
-local open_notification = nil
+local notification_id = nil
+local current_cycle = 0
+local timeout = 0
+local timer = nil
 
 function M.setup(opts)
   -- Merge config with defaults
@@ -34,10 +36,10 @@ function M.setup(opts)
   -- Create command.
   vim.api.nvim_create_user_command("Spotify", function(cmd_opts)
     M._execute(cmd_opts.fargs[1], cmd_opts.fargs[2])
-  end, { nargs = '+', complete = completion })
+  end, { nargs = "+", complete = completion })
 end
 
-local function get_actions ()
+local function get_actions()
   local actions = {
     ["play/pause"] = { M.play_pause, true },
     play = { M.play, true },
@@ -53,7 +55,6 @@ local function get_actions ()
   }
   return actions
 end
-
 
 --- Wrapper around the execution of a spotify command.
 ---
@@ -116,82 +117,82 @@ function M.time(value)
   vim.fn.SpotifyAction("time", value)
 end
 
-function M.status()
-  local current_cycle = 0
-  local status = vim.fn.SpotifyRenderStatus(current_cycle, config.render)
-  if status == vim.NIL then
-    -- There is a bug in pynvim, run the command again.
-    -- Or there was an error when connecting to Spotify.
+local function close_timer()
+  if timer == nil then
     return
   end
-  local opts = {
-    title = "Spotify",
-    on_close = function()
-      -- Reset the value when the notification window is closed.
-      open_notification = nil
-    end,
-    on_open = function()
-      local timer = vim.loop.new_timer()
-      local on_close = function()
-        pcall(timer.stop, timer)
-        pcall(timer.close, timer)
-        open_notification = nil
+  pcall(timer.stop, timer)
+  pcall(timer.close, timer)
+  timer = nil
+end
+
+local function reset()
+  current_cycle = 0
+  notification_id = nil
+end
+
+function M.status()
+  -- If called again, just reset the timer.
+  timeout = config.timeout
+  -- TODO: these should be configurable,
+  -- or maybe autodetect if the user has the plugins installed.
+  -- for now, make it work for both, since they don't conflict (yet).
+  local using_snacks = true
+  local using_notify = true
+
+  -- If there is a timer running, just return,
+  -- since the timeout was reset.
+  if timer ~= nil and timer:get_due_in() > 0 then
+    return
+  end
+
+  -- Just in case?
+  close_timer()
+
+  -- Restart the cycle only if the timer is not running.
+  -- So the user can keep seeing the current status.
+  reset()
+
+  timer = vim.uv.new_timer()
+  timer:start(
+    -- Start the timer immediately.
+    0,
+    -- Repeat after refresh_interval (milliseconds)
+    config.refresh_interval,
+    -- Callback
+    vim.schedule_wrap(function()
+      if timeout <= 0 then
+        close_timer()
+        reset()
+        return
       end
-      --
-      timer:start(
-        -- Start the timer after refresh_interval (milliseconds)
-        config.refresh_interval,
-        -- Repeat after refresh_interval (milliseconds)
-        config.refresh_interval,
-        -- Callback
-        vim.schedule_wrap(function()
-          current_cycle = current_cycle + 1
-          -- TODO: see if we can calculate the cylce speed from the refresh_interval.
-          local status_ = vim.fn.SpotifyRenderStatus(math.floor(current_cycle / config.cycle_speed), config.render)
-          -- If the call failed or if there isn't an open notification, stop the timer.
-          if status_ == vim.NIL or not open_notification then
-            on_close()
-            return
-          end
-          -- TODO: why do we have a timeout?
-          local timeout = config.timeout or 0
-          timeout = timeout - config.refresh_interval * current_cycle
-          if timeout <= 0 then
-            on_close()
-            return
-          end
-          local opts_ = {
-            title = "Spotify",
-            on_close = on_close,
-            hide_from_history = true,
-            -- If there is an open window already, just use that.
-            replace = open_notification,
-            -- TODO: Override the global timeout? Why?
-            -- To avoid an infinite loop? Probably.
-            timeout = timeout,
-          }
-          -- Replace the current notification till the timeout has been completed,
-          -- but we should find a way to reset the timeout once the user triggers
-          -- a new notification.
-          open_notification = vim.notify(status_, vim.log.levels.INFO, opts_)
-        end)
-      )
-    end,
-  }
 
-  -- Set the initial timeout,
-  -- or use the default from the notification.
-  if config.timeout then
-    opts.timeout = config.timeout
-  end
+      local opts = {
+        title = "Spotify",
+        timeout = timeout,
+      }
 
-  -- Avoid opening a new notification
-  -- if there is one already open.
-  if open_notification then
-    opts.hide_from_history = true
-    opts.replace = open_notification
-  end
-  open_notification = vim.notify(status, vim.log.levels.INFO, opts)
+      if using_snacks then
+        opts.id = "spotify-status"
+        opts.ft = "spotify-status"
+      end
+
+      if using_notify then
+        if notification_id ~= nil then
+          opts.replace = notification_id
+          opts.hide_from_history = true
+        end
+        opts.on_close = function()
+          close_timer()
+          reset()
+        end
+      end
+      local status = vim.fn.SpotifyRenderStatus(math.floor(current_cycle / config.cycle_speed), config.render)
+      notification_id = vim.notify(status, vim.log.levels.INFO, opts)
+      current_cycle = current_cycle + 1
+      timeout = timeout - config.refresh_interval
+    end)
+  )
 end
 
 return M
